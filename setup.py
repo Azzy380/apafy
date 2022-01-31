87f167c3326c981f41168dcdcf11fb748aeff444
Azzy380 <59232769+Azzy380@users.noreply.github.com>
2022-01-31 22:07:17 +0100

    Download link update

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     keywords=["pafy", "API", "YouTube", "youtube", "download", "video"],
     author="np1",
     author_email="np1nagev@gmail.com",
-    url="https://github.com/mps-youtube/pafy/",
-    download_url="https://github.com/mps-youtube/pafy/tags",
+    url="https://github.com/Azzy380/pafy-replit-friendly",
+    download_url="https://github.com/Azzy380/pafy-replit-friendly",
     extras_require={
         'youtube-dl-backend': ["youtube-dl"],
         },
